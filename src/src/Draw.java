1f3e6a3d8bbe80f1282839d2665bedab6085bac9
sam <sambass@deloitte.co.uk>
2016-08-30 09:49:50 +0100

    Added dino and robot

---
+++ src/src/Draw.java
@@ -0,0 +1,48 @@
+package src;
+
+public class Draw {
+	public static void createDino() {
+		System.out.println("       __");
+		System.out.println("      /oo\\");
+		System.out.println("     |    |");
+		System.out.println(" ^^  (vvvv)  ^^");
+		System.out.println(" \\\\  /\\__/\\  //");
+		System.out.println("  \\\\/      \\//");
+		System.out.println("   /        \\    ");
+		System.out.println("  |          |    ^");
+		System.out.println("  /          \\___/ | ");
+		System.out.println(" (            )     |");
+		System.out.println("  \\----------/    _/");
+		System.out.println("   //    \\\\_____/");
+		System.out.println("   W      W");
+	}
+	public static void createRobot(){
+
+		System.out.println("   _______                   ________    |");
+		System.out.println("  |ooooooo|      ____       | __  __ |   |");
+		System.out.println("  |[]+++[]|     [____]      |/  \\/  \\|   |");
+		System.out.println("  |+ ___ +|     ]()()[      |\\__/\\__/|   |");
+		System.out.println("  |:|   |:|   ___\\__/___    |[][][][]|   |");
+		System.out.println("  |:|___|:|  |__|    |__|   |++++++++|   |");
+		System.out.println("  |[]===[]|   |_|_/\\_|_|    | ______ |   |");
+		System.out.println("_ ||||||||| _ | | __ | | __ ||______|| __|");
+		System.out.println("  |_______|   |_|[::]|_|    |________|   \\");
+		// Good until this point
+		System.out.println("              \\_|_||_|_/                  \\");
+		System.out.println("	        |_||_|                     \\");
+		System.out.println("               _|_||_|_                     \\");
+		System.out.println("	  ___ |___||___|                     \\");
+		System.out.println("	 /  __\\          ____                 \\");
+		System.out.println("	 \\( oo          (___ \\                 \\");
+		System.out.println("	 _\\_o/           oo~)/");
+		System.out.println("	/ \\|/ \\         _\\-_/_");
+		System.out.println("       / / __\\ \\___    / \\|/  \\");
+		System.out.println("       \\ \\|   |__/_)  / / .- \\ \\");
+		System.out.println("	\\/_)  |       \\ \\ .  /_/");
+		System.out.println("	 ||___|        \\/___(_/");
+		System.out.println("	 | | |          | |  |");
+		System.out.println("	 | | |          | |  |");
+		System.out.println("	 |_|_|          |_|__|");
+		System.out.println("	 [__)_)        (_(___]");
+	}
+}
